3c3b43d647aa8e492b4492d037db7c90454165c1
Shadman_Shakib <100066452+shadman76r@users.noreply.github.com>
2025-08-11 14:45:12 +0600

    library management system start

---
+++ library.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+int main(){
+    cout<<".........................";
+    cout<<"1-Show all the books";
+    cout<<"2-";
+}
